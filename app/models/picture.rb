1f13a19587917cfed972b4382edd2d992c58a86d
App Academy Student <appacademy@Apps-Mac-mini-14.local>
2015-05-11 11:28:31 -0400

    fixed bug

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
-  validates: :title, :url, :user_id, :lat, :lng, presence: true
-  validates: :description, length: { maximum: 140, allow_nil: true }
+  validates :title, :url, :user_id, :lat, :lng, presence: true
+  validates :description, length: { maximum: 140, allow_nil: true }
 
   belongs_to :user
 end
