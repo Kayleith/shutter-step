3b5cfc34b0384cad711921f4a184be67df7b714c
App Academy Student <appacademy@Apps-Mac-mini-15.local>
2015-05-10 21:34:17 -0400

    hopefullt working now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,8 @@
 //= require shutter_step
 // require_tree ../templates
 //= require_tree ./utils
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
+// require_tree ./models
+// require_tree ./collections
+// require_tree ./views
+// require_tree ./routers
 //= require_tree .
